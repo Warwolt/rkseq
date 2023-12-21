c92bdf13930a9bbae103622ccddf4c0de1f0864e
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-22 00:59:33 +0100

    Add some basic gpio code

--- src/main.c
+++ src/main.c
@@ -9,10 +9,89 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-#define ONBOARD_LED 5
+typedef enum {
+	PORT_B,
+	PORT_C,
+	PORT_D
+} port_t;
+
+typedef struct {
+	port_t port;
+	int num;
+} pin_t;
+
+typedef enum {
+	PIN_MODE_INPUT,
+	PIN_MODE_OUTPUT,
+} pin_mode_t;
+
+#define ONBOARD_LED \
+	(pin_t) { .port = PORT_B, .num = 5 }
 
 void globally_enable_interrupts(void) {
 	sei();
+}
+
+void pin_configure(pin_t pin, pin_mode_t mode) {
+	if (mode == PIN_MODE_INPUT) {
+		switch (pin.port) {
+			case PORT_B:
+				clear_bit(DDRB, pin.num);
+				break;
+			case PORT_C:
+				clear_bit(DDRC, pin.num);
+				break;
+			case PORT_D:
+				clear_bit(DDRD, pin.num);
+				break;
+		}
+	}
+
+	if (mode == PIN_MODE_OUTPUT) {
+		switch (pin.port) {
+			case PORT_B:
+				set_bit(DDRB, pin.num);
+				break;
+			case PORT_C:
+				set_bit(DDRC, pin.num);
+				break;
+			case PORT_D:
+				set_bit(DDRD, pin.num);
+				break;
+		}
+	}
+}
+
+void pin_set(pin_t pin) {
+	switch (pin.port) {
+		case PORT_B:
+			set_bit(PORTB, pin.num);
+			break;
+
+		case PORT_C:
+			set_bit(PORTC, pin.num);
+			break;
+
+		case PORT_D:
+			set_bit(PORTD, pin.num);
+			break;
+	}
+}
+
+void pin_clear(pin_t pin) {
+	switch (pin.port) {
+		case PORT_B:
+			clear_bit(PORTB, pin.num);
+			break;
+
+		case PORT_C:
+			clear_bit(PORTC, pin.num);
+			break;
+
+		case PORT_D:
+			clear_bit(PORTD, pin.num);
+			break;
+	}
 }
 
 int main(void) {
@@ -21,12 +100,15 @@ int main(void) {
 	serial_t serial = hw_serial_initialize(9600);
 	logging_initialize(serial);
 
-	set_bit(DDRB, ONBOARD_LED);
-	clear_bit(PORTB, ONBOARD_LED);
-
+	pin_configure(ONBOARD_LED, PIN_MODE_OUTPUT);
 	LOG_INFO("Program Start\n");
 
 	while (true) {
+		pin_set(ONBOARD_LED);
+		LOG_INFO("Tick\n");
+		_delay_ms(1000);
+
+		pin_clear(ONBOARD_LED);
 		LOG_INFO("Tick\n");
 		_delay_ms(1000);
 	}
