c57733a3b5bbfd949b1e00532e4d5e833cb5e751
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-03 01:54:36 +0100

    Add rotary_encoder (#13)
    Add driver for rotary encoder. Use it for tempo select knob

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "hw_serial.h"
 #include "logging.h"
 #include "ringbuffer.h"
+#include "rotary_encoder.h"
 #include "sw_serial.h"
 #include "timer0.h"
 
@@ -11,14 +12,11 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-#define LED_PIN \
-	(gpio_pin_t) { .port = &PORTB, .num = 5 }
-
-#define MIDI_RX_PIN \
-	(gpio_pin_t) { .port = &PORTD, .num = 2 }
-
-#define MIDI_TX_PIN \
-	(gpio_pin_t) { .port = &PORTD, .num = 3 }
+const gpio_pin_t LED_PIN = { .port = &PORTB, .num = 5 };
+const gpio_pin_t MIDI_RX_PIN = { .port = &PORTD, .num = 2 };
+const gpio_pin_t MIDI_TX_PIN = { .port = &PORTD, .num = 3 };
+const gpio_pin_t TEMPO_KNOB_A_PIN = { .port = &PORTD, .num = 4 };
+const gpio_pin_t TEMPO_KNOB_B_PIN = { .port = &PORTD, .num = 5 };
 
 /* ----------------------- Interrupt service routines ----------------------- */
 ISR(TIMER0_OVF_vect) {
@@ -48,17 +46,23 @@ int main(void) {
 	hw_serial_initialize(9600); // uses PD0 and PD1 for logging
 	sw_serial_initialize(31250, MIDI_RX_PIN, MIDI_TX_PIN);
 	gpio_pin_configure(LED_PIN, PIN_MODE_OUTPUT);
+	rotary_encoder_t tempo_knob = rotary_encoder_init(TEMPO_KNOB_A_PIN, TEMPO_KNOB_B_PIN);
 
 	LOG_INFO("Program Start\n");
 	uint32_t last_tick = timer0_now_ms();
 	while (true) {
 		uint32_t now = timer0_now_ms();
-
 		if (now - last_tick >= 1000) {
 			last_tick = now;
 			gpio_pin_set(LED_PIN);
 			LOG_INFO("Tick\n");
 			gpio_pin_clear(LED_PIN);
+		}
+
+		// update rotary encoder
+		int rotary_diff = rotary_encoder_read(&tempo_knob);
+		if (rotary_diff != 0) {
+			LOG_INFO("%d\n", rotary_diff);
 		}
 	}
 }
