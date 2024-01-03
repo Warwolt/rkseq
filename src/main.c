c03d150d5355e60dcb4ad2909b42ed61d783f350
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-03 18:30:57 +0100

    Add init for gpio (#16)

--- src/main.c
+++ src/main.c
@@ -12,12 +12,6 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-const gpio_pin_t LED_PIN = { .port = &PORTB, .num = 5 };
-const gpio_pin_t MIDI_RX_PIN = { .port = &PORTD, .num = 2 };
-const gpio_pin_t MIDI_TX_PIN = { .port = &PORTD, .num = 3 };
-const gpio_pin_t TEMPO_KNOB_A_PIN = { .port = &PORTD, .num = 4 };
-const gpio_pin_t TEMPO_KNOB_B_PIN = { .port = &PORTD, .num = 5 };
-
 /* ----------------------- Interrupt service routines ----------------------- */
 ISR(TIMER0_OVF_vect) {
 	timer0_timer_overflow_irq();
@@ -41,12 +35,19 @@ void globally_enable_interrupts(void) {
 }
 
 int main(void) {
+	const gpio_pin_t led_pin = gpio_pin_init(&PORTB, 5);
+	const gpio_pin_t midi_rx_pin = gpio_pin_init(&PORTD, 2);
+	const gpio_pin_t midi_tx_pin = gpio_pin_init(&PORTD, 3);
+	const gpio_pin_t tempo_knob_a_pin = gpio_pin_init(&PORTD, 4);
+	const gpio_pin_t tempo_knob_b_pin = gpio_pin_init(&PORTD, 5);
+
 	globally_enable_interrupts();
 	timer0_initialize();
 	hw_serial_initialize(9600); // uses PD0 and PD1 for logging
-	sw_serial_initialize(31250, MIDI_RX_PIN, MIDI_TX_PIN);
-	gpio_pin_configure(LED_PIN, PIN_MODE_OUTPUT);
-	rotary_encoder_t tempo_knob = rotary_encoder_init(TEMPO_KNOB_A_PIN, TEMPO_KNOB_B_PIN);
+	sw_serial_initialize(31250, midi_rx_pin, midi_tx_pin);
+
+	gpio_pin_configure(led_pin, PIN_MODE_OUTPUT);
+	rotary_encoder_t tempo_knob = rotary_encoder_init(tempo_knob_a_pin, tempo_knob_b_pin);
 
 	LOG_INFO("Program Start\n");
 	uint32_t last_tick = timer0_now_ms();
@@ -54,12 +55,11 @@ int main(void) {
 		uint32_t now = timer0_now_ms();
 		if (now - last_tick >= 1000) {
 			last_tick = now;
-			gpio_pin_set(LED_PIN);
+			gpio_pin_set(led_pin);
 			LOG_INFO("Tick\n");
-			gpio_pin_clear(LED_PIN);
+			gpio_pin_clear(led_pin);
 		}
 
-		// update rotary encoder
 		int rotary_diff = rotary_encoder_read(&tempo_knob);
 		if (rotary_diff != 0) {
 			LOG_INFO("%d\n", rotary_diff);
