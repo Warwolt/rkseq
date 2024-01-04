d40455e4cc66f817c5f7d6922e2bd0ce0da07f3c
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-04 02:45:32 +0100

    segment_display_set_number

--- src/main.c
+++ src/main.c
@@ -12,11 +12,25 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
+static int8_t digit_segments[10] = {
+	0b00111111, // 0
+	0b00000110, // 1
+	0b11101101, // 2
+	0b11001111, // 3
+	0b11110010, // 4
+	0b00000000, // 5
+	0b00000000, // 6
+	0b00000000, // 7
+	0b00000000, // 8
+	0b00000000, // 9
+};
+
 typedef struct {
 	gpio_pin_t clock_pin;
 	gpio_pin_t latch_pin;
 	gpio_pin_t data_pin;
-	uint8_t digit;
+	uint8_t digits[4];
+	uint8_t current_digit;
 } segment_display_t;
 
 segment_display_t segment_display_init(gpio_pin_t clock_pin, gpio_pin_t latch_pin, gpio_pin_t data_pin) {
@@ -27,13 +41,24 @@ segment_display_t segment_display_init(gpio_pin_t clock_pin, gpio_pin_t latch_pi
 		.clock_pin = clock_pin,
 		.latch_pin = latch_pin,
 		.data_pin = data_pin,
-		.digit = 0,
+		.digits = { 0 },
+		.current_digit = 0,
 	};
+}
+
+void segment_display_set_number(segment_display_t* display, uint16_t number) {
+	display->digits[0] = number / 1 % 10;
+	display->digits[1] = number / 10 % 10;
+	display->digits[2] = number / 100 % 10;
+	display->digits[3] = number / 1000 % 10;
 }
 
 void segment_display_update(segment_display_t* display) {
 	// write segments
-	const int8_t byte = (int8_t)~0b00000011;
+	int8_t byte = 0;
+	if (display->current_digit < 4) {
+		byte = ~digit_segments[display->digits[display->current_digit]];
+	}
 	for (uint8_t i = 0; i < 8; i++) {
 		const uint8_t bit = (byte >> ((8 - 1) - i)) & 1;
 		gpio_pin_write(display->data_pin, bit);
@@ -42,7 +67,7 @@ void segment_display_update(segment_display_t* display) {
 	}
 	// select digit
 	for (uint8_t i = 0; i < 8; i++) {
-		const uint8_t bit = ((0x1 << display->digit) >> ((8 - 1) - i)) & 1;
+		const uint8_t bit = ((0x1 << display->current_digit) >> ((8 - 1) - i)) & 1;
 		gpio_pin_write(display->data_pin, bit);
 		gpio_pin_set(display->clock_pin);
 		gpio_pin_clear(display->clock_pin);
@@ -51,7 +76,7 @@ void segment_display_update(segment_display_t* display) {
 	gpio_pin_set(display->latch_pin);
 	gpio_pin_clear(display->latch_pin);
 
-	display->digit = (display->digit + 1) % 16;
+	display->current_digit = (display->current_digit + 1) % 16;
 }
 
 /* ----------------------- Interrupt service routines ----------------------- */
@@ -82,23 +107,24 @@ int main(void) {
 	const gpio_pin_t midi_tx_pin = gpio_pin_init(&PORTD, 3);
 	const gpio_pin_t tempo_knob_a_pin = gpio_pin_init(&PORTD, 4);
 	const gpio_pin_t tempo_knob_b_pin = gpio_pin_init(&PORTD, 5);
+	const gpio_pin_t display_clock_pin = gpio_pin_init(&PORTD, 6);
+	const gpio_pin_t display_latch_pin = gpio_pin_init(&PORTD, 7);
+	const gpio_pin_t display_data_pin = gpio_pin_init(&PORTB, 0);
+	gpio_pin_configure(led_pin, PIN_MODE_OUTPUT);
 
 	globally_enable_interrupts();
 	timer0_initialize();
 	hw_serial_initialize(9600); // uses PD0 and PD1 for logging
 	sw_serial_initialize(31250, midi_rx_pin, midi_tx_pin);
 
-	// segment display
-	const gpio_pin_t display_clock_pin = gpio_pin_init(&PORTD, 6);
-	const gpio_pin_t display_latch_pin = gpio_pin_init(&PORTD, 7);
-	const gpio_pin_t display_data_pin = gpio_pin_init(&PORTB, 0);
-
-	gpio_pin_configure(led_pin, PIN_MODE_OUTPUT);
 	rotary_encoder_t tempo_knob = rotary_encoder_init(tempo_knob_a_pin, tempo_knob_b_pin);
 	segment_display_t tempo_display = segment_display_init(display_clock_pin, display_latch_pin, display_data_pin);
 
 	LOG_INFO("Program Start\n");
 	uint32_t last_tick = timer0_now_ms();
+
+	segment_display_set_number(&tempo_display, 0);
+
 	while (true) {
 		uint32_t now = timer0_now_ms();
 		if (now - last_tick >= 1000) {
