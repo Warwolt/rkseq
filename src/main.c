1ec379892b87642857e96cfd51141ae95cde19c5
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-04 16:27:46 +0100

    Quarternote output on LED (#18)
    Add simple quarter note pulsing on LED.
    Add start/stop button for playback.

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 
 #define MIN_BPM 40
 #define MAX_BPM 200
+#define QUARTERNOTE_PULSE_LENGTH_MS 2
 
 /* ----------------------- Interrupt service routines ----------------------- */
 ISR(TIMER0_OVF_vect) {
@@ -48,26 +49,65 @@ int main(void) {
 	const gpio_pin_t display_clock_pin = gpio_pin_init(&PORTD, 6);
 	const gpio_pin_t display_latch_pin = gpio_pin_init(&PORTD, 7);
 	const gpio_pin_t display_data_pin = gpio_pin_init(&PORTB, 0);
+	const gpio_pin_t start_stop_button_pin = gpio_pin_init(&PORTB, 1);
 
 	globally_enable_interrupts();
 	timer0_initialize();
 	hw_serial_initialize(9600); // uses PD0 and PD1 for logging
 	sw_serial_initialize(31250, midi_rx_pin, midi_tx_pin);
 	gpio_pin_configure(led_pin, PIN_MODE_OUTPUT);
+	gpio_pin_configure(start_stop_button_pin, PIN_MODE_INPUT);
 
 	rotary_encoder_t tempo_knob = rotary_encoder_init(tempo_knob_a_pin, tempo_knob_b_pin);
 	segment_display_t tempo_display = segment_display_init(display_clock_pin, display_latch_pin, display_data_pin);
 
 	LOG_INFO("Program Start\n");
+	// Tempo
+	uint64_t last_bpm_tick_us = timer0_now_us();
+	uint64_t last_pulse_tick_us = timer0_now_us();
 	uint8_t tempo_bpm = 120;
+	uint64_t quarternote_period_us = (60 * 1e6) / tempo_bpm;
+	// Start stop button
+	// TODO: add a button_t struct that tracks button presses and does debouncing
+	pin_state_t button_was_pressed = 0;
+	bool playback_started = false;
 	while (true) {
-		// update tempo
-		int rotary_diff = rotary_encoder_read(&tempo_knob);
-		tempo_bpm = clamp(tempo_bpm + rotary_diff, MIN_BPM, MAX_BPM);
+		const uint64_t now_us = timer0_now_us();
 
-		// display tempo
+		/* Update playback */
+		bool playback_just_started = false;
+		const pin_state_t button_pressed = gpio_pin_read(start_stop_button_pin);
+		if (!button_was_pressed && button_pressed) {
+			playback_started = !playback_started;
+			if (playback_started) {
+				playback_just_started = true;
+			}
+		}
+		button_was_pressed = button_pressed;
+
+		/* Update Tempo */
+		const int rotary_diff = rotary_encoder_read(&tempo_knob);
+		tempo_bpm = clamp(tempo_bpm + rotary_diff, MIN_BPM, MAX_BPM);
+		quarternote_period_us = (60 * 1e6) / tempo_bpm;
+
+		/* Display Current Tempo*/
 		segment_display_set_number(&tempo_display, tempo_bpm * 10);
 		segment_display_enable_period(&tempo_display, 1);
 		segment_display_update(&tempo_display);
+
+		/* Output tempo pulse */
+		// TODO: clean this up with some kind of "stopwatch_t" struct to help
+		// with these kinds of events that are separated by some amount of time.
+		if (playback_started) {
+			if (now_us - last_bpm_tick_us >= quarternote_period_us || playback_just_started) {
+				last_bpm_tick_us = now_us;
+
+				gpio_pin_set(led_pin);
+				last_pulse_tick_us = now_us;
+			}
+			if (now_us - last_pulse_tick_us >= (QUARTERNOTE_PULSE_LENGTH_MS * 1e3)) {
+				gpio_pin_clear(led_pin);
+			}
+		}
 	}
 }
