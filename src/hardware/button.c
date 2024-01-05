c7c1d50372318774971f8c4d7bac4496303ec040
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-05 01:51:42 +0100

    Working debounce

--- src/hardware/button.c
+++ src/hardware/button.c
@@ -1,36 +1,23 @@
 #include "hardware/button.h"
 
-// static uint32_t button_filter_state(button_t* button, button_state_t state, uint32_t time_now_ms) {
-// 	const uint32_t state_stable = time_now_ms - button->last_stable_signal_ms >= BUTTON_DEBOUNCE_MS;
-// 	button_state_t filtered_state;
-// 	if (state_changed && state_stable) {
-// 		filtered_state = state;
-// 		button->last_stable_signal_ms = time_now_ms;
-// 	} else {
-// 		filtered_state = button->state;
-// 	}
-// }
+static uint32_t button_debounce_state(button_t* button, button_state_t state, uint32_t time_now_ms) {
+	const uint32_t state_stable = time_now_ms - button->last_stable_signal_ms >= BUTTON_DEBOUNCE_MS;
+	if (state_stable) {
+		button->last_stable_signal_ms = time_now_ms;
+		return state;
+	} else {
+		return button->state;
+	}
+}
 
 button_t button_init(void) {
 	return (button_t) { 0 };
 }
 
 void button_update(button_t* button, button_state_t state, uint32_t time_now_ms) {
-	const bool state_changed = state != button->state;
-
-	/* Debounce incoming signal */
-	const uint32_t state_stable = time_now_ms - button->last_stable_signal_ms >= BUTTON_DEBOUNCE_MS;
-	button_state_t filtered_state;
-	if (state_changed && state_stable) {
-		filtered_state = state;
-		button->last_stable_signal_ms = time_now_ms;
-	} else {
-		filtered_state = button->state;
-	}
-
-	/* Update button state */
-	button->changed_now = button->state != filtered_state;
-	button->state = filtered_state;
+	const button_state_t debounced_state = button_debounce_state(button, state, time_now_ms);
+	button->changed_now = button->state != debounced_state;
+	button->state = debounced_state;
 }
 
 bool button_is_pressed(const button_t* button) {
