7aeccc23014568ad1adac71cb88889c0037ba44a
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-05 02:05:05 +0100

    refactor

--- src/hardware/button.c
+++ src/hardware/button.c
@@ -1,9 +1,9 @@
 #include "hardware/button.h"
 
-static uint32_t button_debounce_state(button_t* button, button_state_t state, uint32_t time_now_ms) {
+static uint32_t debounce_input_state(button_t* button, button_state_t input_state, uint32_t time_now_ms) {
 	if (time_now_ms - button->last_stable_signal_ms >= BUTTON_DEBOUNCE_MS) {
 		button->last_stable_signal_ms = time_now_ms;
-		return state;
+		return input_state;
 	} else {
 		return button->state;
 	}
@@ -14,7 +14,7 @@ button_t button_init(void) {
 }
 
 void button_update(button_t* button, button_state_t state, uint32_t time_now_ms) {
-	const button_state_t debounced_state = button_debounce_state(button, state, time_now_ms);
+	const button_state_t debounced_state = debounce_input_state(button, state, time_now_ms);
 	button->changed_now = button->state != debounced_state;
 	button->state = debounced_state;
 }
