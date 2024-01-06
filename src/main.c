5539a7bc020c8d5040f619b367a5de0942eb14e5
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-06 16:38:58 +0100

    beat_clock.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "beat_clock.h"
 #include "data/ring_buffer.h"
 #include "hardware/button.h"
 #include "hardware/gpio.h"
@@ -16,51 +17,7 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-#define MIN_BPM 40
-#define MAX_BPM 200
 #define QUARTERNOTE_PULSE_LENGTH_US 500
-
-typedef struct {
-	uint8_t tempo_bpm;
-	bool is_playing;
-	bool just_started;
-	bool quarternote_elapsed;
-	usec_timer_t timer;
-} beat_clock_t;
-
-beat_clock_t beat_clock_init(uint8_t tempo_bpm) {
-	return (beat_clock_t) {
-		.tempo_bpm = clamp(tempo_bpm, MIN_BPM, MAX_BPM),
-		.timer = usec_timer_init((60 * 1e6) / tempo_bpm)
-	};
-}
-
-void beat_clock_set_tempo(beat_clock_t* beat_clock, uint8_t tempo_bpm) {
-	beat_clock->tempo_bpm = clamp(tempo_bpm, MIN_BPM, MAX_BPM);
-	beat_clock->timer.period_us = (60 * 1e6) / tempo_bpm;
-}
-
-void beat_clock_start(beat_clock_t* beat_clock) {
-	beat_clock->is_playing = true;
-	beat_clock->just_started = true;
-}
-
-void beat_clock_stop(beat_clock_t* beat_clock) {
-	beat_clock->is_playing = false;
-	beat_clock->just_started = false;
-}
-
-void beat_clock_update(beat_clock_t* beat_clock) {
-	beat_clock->quarternote_elapsed = beat_clock->just_started || (beat_clock->is_playing && usec_timer_period_has_elapsed(&beat_clock->timer));
-	beat_clock->just_started = false;
-	if (beat_clock->quarternote_elapsed) {
-		usec_timer_reset(&beat_clock->timer);
-	}
-}
-
-bool beat_clock_should_output_quarternote(const beat_clock_t* beat_clock) {
-	return beat_clock->quarternote_elapsed;
-}
 
 /* ----------------------- Interrupt service routines ----------------------- */
 ISR(TIMER0_OVF_vect) {
