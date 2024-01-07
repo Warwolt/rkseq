504e1b5dce9a63ba30be1eafd0ce5929c851743e
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-07 02:09:56 +0100

    refactor

--- src/main.c
+++ src/main.c
@@ -52,31 +52,30 @@ int main(void) {
 	const gpio_pin_t display_clock_pin = gpio_pin_init(&PORTD, 6);
 	const gpio_pin_t display_latch_pin = gpio_pin_init(&PORTD, 7);
 	const gpio_pin_t display_data_pin = gpio_pin_init(&PORTB, 0);
+	const gpio_pin_t start_stop_button_pin = gpio_pin_init(&PORTB, 1);
+	gpio_pin_configure(led_pin, PIN_MODE_OUTPUT);
+	gpio_pin_configure(start_stop_button_pin, PIN_MODE_INPUT);
 
 	globally_enable_interrupts();
 	timer0_initialize();
 	hw_serial_initialize(9600); // uses PD0 and PD1 for logging
 	sw_serial_initialize(31250, midi_rx_pin, midi_tx_pin);
-	gpio_pin_configure(led_pin, PIN_MODE_OUTPUT);
-
-	const gpio_pin_t start_stop_button_pin = gpio_pin_init(&PORTB, 1);
-	gpio_pin_configure(start_stop_button_pin, PIN_MODE_INPUT);
 
 	playback_control_t playback_control = {
 		.start_button = button_init(),
 		.tempo_knob = rotary_encoder_init(tempo_knob_a_pin, tempo_knob_b_pin),
 		.tempo_display = segment_display_init(display_clock_pin, display_latch_pin, display_data_pin),
 	};
+	beat_clock_t beat_clock = beat_clock_init(120);
+	usec_timer_t pulse_timer = usec_timer_init(QUARTERNOTE_PULSE_LENGTH_US);
 
 	/* Run */
 	LOG_INFO("Program Start\n");
-	beat_clock_t beat_clock = beat_clock_init(120);
-	usec_timer_t pulse_timer = usec_timer_init(QUARTERNOTE_PULSE_LENGTH_US);
 	while (true) {
-		/* Update buttons */
+		/* Update button states */
 		button_update(&playback_control.start_button, gpio_pin_read(start_stop_button_pin), timer0_now_ms());
 
-		/* Update playback */
+		/* Update sequencer playback */
 		beat_clock_update(&beat_clock);
 		playback_control_update(&playback_control, &beat_clock);
 
