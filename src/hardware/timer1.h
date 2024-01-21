d9a2952bac17e551fbe795cd30ff6f25b08936df
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-21 21:58:23 +0100

    Add handle for timer1 (#41)

--- src/hardware/timer1.h
+++ src/hardware/timer1.h
@@ -5,9 +5,13 @@
 
 #define TIMER1_USEC_PER_TICK 0.5f
 
-void Timer1_init(void);
-void Timer1_set_period(uint16_t ticks);
-void Timer1_start(void);
-void Timer1_stop(void);
+typedef struct {
+	int: 0;
+} Timer1;
+
+Timer1 Timer1_init(void);
+void Timer1_set_period(Timer1 timer1, uint16_t ticks);
+void Timer1_start(Timer1 timer1);
+void Timer1_stop(Timer1 timer1);
 
 #endif /* TIMER_1_H */
--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 #include "util/bits.h"
 #include "util/math.h"
 #include "util/microsecond_timer.h"
+#include "util/unused.h"
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
@@ -92,19 +93,19 @@ static UserInterfaceInput read_ui_input(RotaryEncoder* rotary_encoder) {
 	};
 }
 
-static void set_playback_tempo(BeatClock* beat_clock, uint8_t bpm) {
+static void set_playback_tempo(BeatClock* beat_clock, Timer1 timer1, uint8_t bpm) {
 	beat_clock->tempo_bpm = bpm;
-	Timer1_set_period((1e6 * 60) / (BEAT_CLOCK_SEQUENCER_PPQN * bpm) / TIMER1_USEC_PER_TICK);
+	Timer1_set_period(timer1, (1e6 * 60) / (BEAT_CLOCK_SEQUENCER_PPQN * bpm) / TIMER1_USEC_PER_TICK);
 }
 
-static void start_playback(BeatClock* beat_clock) {
+static void start_playback(BeatClock* beat_clock, Timer1 timer1) {
 	BeatClock_start(beat_clock);
-	Timer1_start();
+	Timer1_start(timer1);
 }
 
-static void stop_playback(BeatClock* beat_clock) {
+static void stop_playback(BeatClock* beat_clock, Timer1 timer1) {
 	BeatClock_stop(beat_clock);
-	Timer1_stop();
+	Timer1_stop(timer1);
 }
 
 static void update_segment_display(SegmentDisplay* segment_display, char* chars) {
@@ -126,17 +127,18 @@ int main(void) {
 	const GpioPin display_latch_pin = GpioPin_init(&PORTD, 7);
 	const GpioPin display_clock_pin = GpioPin_init(&PORTB, 0);
 	const GpioPin step_buttons_latch_pin = GpioPin_init(&PORTB, 1);
-	// const GpioPin step_leds_latch_pin = GpioPin_init(&PORTB, 2);
+	const GpioPin step_leds_latch_pin = GpioPin_init(&PORTB, 2);
 
 	globally_enable_interrupts();
 	Timer0_init();
-	Timer1_init();
+	Timer1 timer1 = Timer1_init();
 	HardwareSerial_init(9600); // uses PD0 and PD1
 	SoftwareSerial_init(31250, midi_rx_pin, midi_tx_pin);
 	Spi spi = Spi_init(SPI_DATA_ORDER_MSB_FIRST); // uses PB3, PB4 and PB5
 
 	ShiftRegister step_buttons_shift_reg = ShiftRegister_init(spi, step_buttons_latch_pin);
-	// ShiftRegister step_leds_shift_reg = ShiftRegister_init(spi, step_leds_latch_pin);
+	ShiftRegister step_leds_shift_reg = ShiftRegister_init(spi, step_leds_latch_pin);
+	UNUSED(step_leds_shift_reg);
 	RotaryEncoder rotary_encoder = RotaryEncoder_init(encoder_a_pin, encoder_b_pin);
 	SegmentDisplay segment_display = SegmentDisplay_init(display_clock_pin, display_latch_pin, display_data_pin);
 	Button step_buttons[16];
@@ -149,8 +151,8 @@ int main(void) {
 
 	/* Run */
 	LOG_INFO("Program Start\n");
-	set_playback_tempo(&beat_clock, DEFAULT_BPM);
-	start_playback(&beat_clock);
+	set_playback_tempo(&beat_clock, timer1, DEFAULT_BPM);
+	start_playback(&beat_clock, timer1);
 	while (true) {
 		/* Input */
 		update_button_states(step_buttons, 8, &step_buttons_shift_reg);
@@ -159,13 +161,13 @@ int main(void) {
 		/* Update */
 		const UserInterfaceEvents ui_events = UserInterface_update(&user_interface, &ui_input, &beat_clock);
 		if (ui_events.new_tempo_bpm) {
-			set_playback_tempo(&beat_clock, ui_events.new_tempo_bpm);
+			set_playback_tempo(&beat_clock, timer1, ui_events.new_tempo_bpm);
 		}
 		if (ui_events.start_playback) {
-			start_playback(&beat_clock);
+			start_playback(&beat_clock, timer1);
 		}
 		if (ui_events.stop_playback) {
-			stop_playback(&beat_clock);
+			stop_playback(&beat_clock, timer1);
 		}
 
 		/* Output */
