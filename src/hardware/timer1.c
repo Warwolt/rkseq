be8f9f5b02eb7640270043074dc43e769cbfa451
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-21 21:44:35 +0100

    Add handle for timer1

--- src/hardware/timer1.c
+++ src/hardware/timer1.c
@@ -2,23 +2,24 @@
 
 #include "util/bits.h"
 
-void Timer1_init(void) {
+Timer1 Timer1_init(void) {
 	set_bit(TIMSK1, OCIE1A); // Enable "Output Compare A Match" interrupts
 	set_bit(TCCR1B, WGM12); // Clear counter on "Compare A Match"
+	return (Timer1) {};
 }
 
-void Timer1_set_period(uint16_t ticks) {
+void Timer1_set_period(Timer1 timer1, uint16_t ticks) {
 	OCR1A = ticks;
 }
 
-void Timer1_start(void) {
+void Timer1_start(Timer1 timer1) {
 	// Start timer1 by connecting to CPU clock via prescaler 8
 	clear_bit(TCCR1B, CS10);
 	set_bit(TCCR1B, CS11);
 	clear_bit(TCCR1B, CS12);
 }
 
-void Timer1_stop(void) {
+void Timer1_stop(Timer1 timer1) {
 	// Stop timer1 by disconnecting the CPU clock
 	clear_bit(TCCR1B, CS10);
 	clear_bit(TCCR1B, CS11);
