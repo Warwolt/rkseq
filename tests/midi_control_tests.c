271115b387423d109519fa8fe712aef67f48893a
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-22 23:02:09 +0100

    Add helper expect_eq macro

--- tests/midi_control_tests.c
+++ tests/midi_control_tests.c
@@ -2,12 +2,16 @@
 
 #include "sequencer/midi_control.h"
 
+#define EXPECT_MIDI_CONTROL_EVENTS_EQ(lhs, rhs)                            \
+	EXPECT_EQ(lhs.switch_to_internal_clock, rhs.switch_to_internal_clock); \
+	EXPECT_EQ(lhs.switch_to_external_clock, rhs.switch_to_external_clock);
+
 TEST(MidiControl_tests, empty_input_gives_no_events) {
 	MidiControl midi_control = MidiControl_init();
 	const uint8_t midi_byte = 0;
 
-	MidiControlEvents events = MidiControl_update(&midi_control, midi_byte);
+	const MidiControlEvents events = MidiControl_update(&midi_control, midi_byte);
 
-	EXPECT_FALSE(events.switch_to_internal_clock);
-	EXPECT_FALSE(events.switch_to_external_clock);
+	const MidiControlEvents expected = { 0 };
+	EXPECT_MIDI_CONTROL_EVENTS_EQ(events, expected);
 }
