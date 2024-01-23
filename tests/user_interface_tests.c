d7e7525d9746ac774e5e6b377854dc9fa9c328d8
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-23 23:17:15 +0100

    Add dash test

--- tests/user_interface_tests.c
+++ tests/user_interface_tests.c
@@ -11,8 +11,8 @@ TEST(UserInterface, shows_dashes_when_external_beat_clock_source) {
 	step_sequencer.beat_clock.source = BEAT_CLOCK_SOURCE_EXTERNAL;
 	UserInterface_update(&user_interface, &input, &step_sequencer);
 
-	EXPECT_EQ(user_interface.segment_display_chars[0], '-');
-	EXPECT_EQ(user_interface.segment_display_chars[1], '-');
-	EXPECT_EQ(user_interface.segment_display_chars[2], '-');
-	EXPECT_EQ(user_interface.segment_display_chars[3], '-');
+	EXPECT_CHAR_EQ(user_interface.segment_display_chars[0], '-');
+	EXPECT_CHAR_EQ(user_interface.segment_display_chars[1], '-');
+	EXPECT_CHAR_EQ(user_interface.segment_display_chars[2], '-');
+	EXPECT_CHAR_EQ(user_interface.segment_display_chars[3], '-');
 }
