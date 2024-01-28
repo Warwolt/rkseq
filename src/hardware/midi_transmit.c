3451cf52d0b3570e9cfea6e46e731c08e7d6d913
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-01-29 00:52:40 +0100

    Require handle for all SoftwareSerial functions

--- src/hardware/midi_transmit.c
+++ src/hardware/midi_transmit.c
@@ -3,21 +3,21 @@
 #include "data/midi.h"
 #include "hardware/software_serial.h"
 
-void MidiTransmit_send_message(MidiMessage msg) {
+void MidiTransmit_send_message(SoftwareSerial sw_serial, MidiMessage msg) {
 	switch (msg.type) {
 		case MIDI_MESSAGE_TYPE_TIMING_CLOCK:
-			SoftwareSerial_write(0xFA);
+			SoftwareSerial_write(sw_serial, 0xFA);
 
 		case MIDI_MESSAGE_TYPE_NOTE_ON:
-			SoftwareSerial_write(MIDI_NOTE_ON(msg.data.note_on.channel));
-			SoftwareSerial_write(msg.data.note_on.note_number);
-			SoftwareSerial_write(msg.data.note_on.velocity);
+			SoftwareSerial_write(sw_serial, MIDI_NOTE_ON(msg.data.note_on.channel));
+			SoftwareSerial_write(sw_serial, msg.data.note_on.note_number);
+			SoftwareSerial_write(sw_serial, msg.data.note_on.velocity);
 			break;
 
 		case MIDI_MESSAGE_TYPE_NOTE_OFF:
-			SoftwareSerial_write(MIDI_NOTE_OFF(msg.data.note_off.channel));
-			SoftwareSerial_write(msg.data.note_off.note_number);
-			SoftwareSerial_write(0); // velocity
+			SoftwareSerial_write(sw_serial, MIDI_NOTE_OFF(msg.data.note_off.channel));
+			SoftwareSerial_write(sw_serial, msg.data.note_off.note_number);
+			SoftwareSerial_write(sw_serial, 0); // velocity
 			break;
 	}
 }
