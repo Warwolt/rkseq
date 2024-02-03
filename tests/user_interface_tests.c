4391b6172eeed3eda7b0f1f072142286162bef40
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-02-03 23:42:20 +0100

    Rename input to events

--- tests/user_interface_tests.c
+++ tests/user_interface_tests.c
@@ -15,17 +15,17 @@
 	}
 
 TEST(UserInterface, shows_dashes_on_display_when_beat_clock_source_is_external) {
-	const UserInterfaceEvents input = { 0 };
+	const UserInterfaceEvents events = { 0 };
 	UserInterface user_interface = UserInterface_init();
 	StepSequencer step_sequencer = StepSequencer_init();
 
 	// should show BPM
 	step_sequencer.beat_clock.source = BEAT_CLOCK_SOURCE_INTERNAL;
-	UserInterface_update(&user_interface, &input, &step_sequencer);
+	UserInterface_update(&user_interface, &events, &step_sequencer);
 
 	// should now switch to just dashes
 	step_sequencer.beat_clock.source = BEAT_CLOCK_SOURCE_EXTERNAL;
-	UserInterface_update(&user_interface, &input, &step_sequencer);
+	UserInterface_update(&user_interface, &events, &step_sequencer);
 
 	const char expected_chars[5] = "----";
 	const bool expected_periods[4] = { false, false, false, false };
@@ -34,14 +34,14 @@ TEST(UserInterface, shows_dashes_on_display_when_beat_clock_source_is_external) 
 }
 
 TEST(UserInterface, shows_bpm_on_display_when_beat_clock_source_is_internal) {
-	const UserInterfaceEvents input = { 0 };
+	const UserInterfaceEvents events = { 0 };
 	UserInterface user_interface = UserInterface_init();
 	StepSequencer step_sequencer = StepSequencer_init();
 
 	// set BPM to 120.3
 	step_sequencer.beat_clock.source = BEAT_CLOCK_SOURCE_INTERNAL;
 	step_sequencer.beat_clock.tempo_deci_bpm = 1203;
-	UserInterface_update(&user_interface, &input, &step_sequencer);
+	UserInterface_update(&user_interface, &events, &step_sequencer);
 
 	const char expected_chars[5] = "1203";
 	const bool expected_periods[4] = { false, false, true, false };
@@ -50,25 +50,25 @@ TEST(UserInterface, shows_bpm_on_display_when_beat_clock_source_is_internal) {
 }
 
 TEST(UserInterface, rotary_encoder_changes_tempo_when_clock_source_is_internal) {
-	const UserInterfaceEvents input = { .rotary_encoder_diff = 1 };
+	const UserInterfaceEvents events = { .rotary_encoder_diff = 1 };
 	UserInterface user_interface = UserInterface_init();
 	StepSequencer step_sequencer = StepSequencer_init();
 
 	step_sequencer.beat_clock.source = BEAT_CLOCK_SOURCE_INTERNAL;
 	step_sequencer.beat_clock.tempo_deci_bpm = 1200;
-	UserInterfaceCommands commands = UserInterface_update(&user_interface, &input, &step_sequencer);
+	UserInterfaceCommands commands = UserInterface_update(&user_interface, &events, &step_sequencer);
 
 	EXPECT_EQ(commands.set_new_tempo_deci_bpm, 1210);
 }
 
 TEST(UserInterface, rotary_encoder_does_not_change_tempo_when_clock_source_is_external) {
-	const UserInterfaceEvents input = { .rotary_encoder_diff = 1 };
+	const UserInterfaceEvents events = { .rotary_encoder_diff = 1 };
 	UserInterface user_interface = UserInterface_init();
 	StepSequencer step_sequencer = StepSequencer_init();
 
 	step_sequencer.beat_clock.source = BEAT_CLOCK_SOURCE_EXTERNAL;
 	step_sequencer.beat_clock.tempo_deci_bpm = 1200;
-	UserInterfaceCommands commands = UserInterface_update(&user_interface, &input, &step_sequencer);
+	UserInterfaceCommands commands = UserInterface_update(&user_interface, &events, &step_sequencer);
 
 	EXPECT_EQ(commands.set_new_tempo_deci_bpm, 0);
 }
