093d5ccec6a719e2847fadca045bc3438f48b634
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-03-11 00:14:16 +0100

    Format

--- src/util/array.h
+++ src/util/array.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 
 #define array_length(x) (sizeof(x) / sizeof((x)[0]))
-#define clear_array(x) for (size_t i = 0; i < array_length(x); i++) { x[i] = 0; }
+#define clear_array(x)                             \
+	for (size_t i = 0; i < array_length(x); i++) { \
+		x[i] = 0;                                  \
+	}
 
 #endif /* ARRAY_H */
